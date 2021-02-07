e2d317d3185cf22078226a05034dc6f5e1f6cfb2
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-02-07 10:55:33 +0000

    Updated Emoji TableView

---
+++ Emoji Dictionary/Model/Emoji.swift
@@ -0,0 +1,22 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Jonathan Burnett on 07/02/2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
