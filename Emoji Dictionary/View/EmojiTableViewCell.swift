3ce4349cff8175b21af91812c0c459ae08bd9558
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-02-13 10:08:58 +0000

    Finished Project

--- Emoji Dictionary/View/EmojiTableViewCell.swift
+++ Emoji Dictionary/View/EmojiTableViewCell.swift
@@ -8,29 +8,22 @@
 import UIKit
 
 class EmojiTableViewCell: UITableViewCell {
+    
+    @IBOutlet var symbolLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        
     }
-    
-    @IBOutlet weak var symbolLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
     func update(with emoji: Emoji) {
         symbolLabel.text = emoji.symbol
         nameLabel.text = emoji.name
-        descriptionLabel.text = emoji.description
+        descriptionLabel.text = emoji.detailDescription
     }
-    
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
-
